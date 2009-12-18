200355b6eda0437085dcf9fc32ba2a5ad4660dc5
Eric Davis <edavis@littlestreamsoftware.com>
2009-12-18 14:40:21 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_event_notification_override do
+  name 'Redmine Event Notification Override plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
