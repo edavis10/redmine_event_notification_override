66bfa020fbfa8204044d3f3b8d16e15d94d81269
Eric Davis <edavis@littlestreamsoftware.com>
2009-12-18 15:04:28 -0800

    [#3428] Hooked up tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper
 
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
+
+# Helpers
+class Test::Unit::TestCase
+  def configure_plugin(fields={})
+    Setting.plugin_redmine_event_notification_override = fields.stringify_keys
+  end
+
+  def setup_plugin_configuration
+    configure_plugin({
+                     })
+  end
+end
